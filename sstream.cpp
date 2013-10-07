c50574b1a70d5e483a03c5c5b8f04fa5e377a582
praveendhinwa <praveendhinwa@gmail.com>
2013-10-07 12:28:22 +0530

    Create sstream.cpp

---
+++ sstream.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string toString (int n)
+{
+    ostringstream os;
+    os << n;
+
+    return os.str();
+}
+
+int toInt (string s)
+{
+    istringstream is (s);
+    int n;
+
+    is >> n;
+
+    return n;
+}
+
+
+int main()
+{
+    // use itoa or atoi
+
+
+    return 0;
+}
+
+
