44b0a9013f5fed6b67fffc3a8edd6e65eac8f5cd
praveendhinwa <praveendhinwa@gmail.com>
2013-10-07 12:27:33 +0530

    Create pair.cpp

---
+++ pair.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    pair <int, int> p;
+    p =  (3, 5);
+
+    cout << p.first << " " << p.second << endl;
+
+    return 0;
+}
+
+
