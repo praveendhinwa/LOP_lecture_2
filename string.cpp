958103229e16c942be46080538034404b5e30502
praveendhinwa <praveendhinwa@gmail.com>
2013-10-07 12:28:08 +0530

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    sort (s.begin(), s.end());
+
+    cout << s.substr (i, len) << endl;
+
+    reverse (s.begin(), s.end());
+
+    return 0;
+}
+
+
